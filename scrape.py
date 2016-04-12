7ae582a44dcd379ddd8d1c5d69cbd75449ec1c75
miscoined <miscoined@gmail.com>
2016-04-12 13:39:21 +1000

    Fix SQL database generation and add verbosity

--- scrape.py
+++ scrape.py
@@ -8,7 +8,10 @@ import sqlite3
 
 from re import match
 from json import loads as json_loads
+from os.path import isfile
 from urllib.request import Request, urlopen
+
+VERBOSE = True
 
 GH_HOST = "GH"
 GH_URL = "https://api.github.com/repositories?q=per_page=100"
@@ -16,29 +19,43 @@ BB_HOST = "BB"
 BB_URL = "https://api.bitbucket.org/2.0/repositories"
 
 ITEMS_REQUIRED = 600
-COLS = ["name TEXT NOT NULL", "description TEXT"]
+items_per_host = int(ITEMS_REQUIRED / 2)
+COLS = ["id INTEGER PRIMARY KEY NOT NULL", "host TEXT NOT NULL"]
+VAR_COLS = ["name TEXT", "description TEXT"]
 
+TABLE_NAME = "Repositories"
+DB_PATH = "repositories.db"
+
+# remove any existing database
+if isfile(DB_PATH):
+  if VERBOSE: print("Removing existing database file...")
+  os.remove(DB_PATH)
+
+if VERBOSE: print("Creating new database file '{}'...".format(DB_PATH))
 # connect and create the initial table
-db = sqlite3.connect("repositories.db")
-db.execute("""CREATE TABLE Repositories
-(id INTEGER PRIMARY KEY, host TEXT NOT NULL, ?)""", ", ".join(COLS))
+db = sqlite3.connect(DB_PATH)
+db.execute("CREATE TABLE Repositories ({})".format(", ".join(COLS + VAR_COLS)))
 
 
 def add_repo(repo, host):
   """Clean an entry and add it to the database
 
-  Clean by removing unwanted keys, assuming wanted keys are only those in COLS
+  Clean by removing unwanted keys, assuming wanted keys are only those in VAR_COLS
   also add a column specifying the host.
   """
 
   # remove unwanted keys
-  for key in set(repo.keys()) - set(i.split(" ")[0] for i in COLS):
+  for key in set(repo.keys()) - set(i.split(" ")[0] for i in VAR_COLS):
     del repo[key]
+
   # add a key specifying the host
   repo["host"] = host
-
   # add entry into database
-  db.executemany("INSERT INTO Repositories VALUES (?,?,?)", repo.items())
+  query = "INSERT INTO {} VALUES (NULL,{})".format(
+    TABLE_NAME, ",".join(list("?"*len(repo))))
+  # get a list of each value in the order they need to be passed to the query
+  ordered_values = [repo[i.split(" ")[0]] for i in COLS[1:] + VAR_COLS]
+  db.execute(query, ordered_values)
 
 
 def next_page(url, host):
@@ -51,8 +68,9 @@ def next_page(url, host):
   if host == GH_HOST:
     req.add_header("Accept", "application/vnd.github.v3+json")
 
+  response = urlopen(req)
   # parse the response from json
-  page = json_loads(urlopen(req).read().decode())
+  page = json_loads(response.read().decode())
 
   if host == GH_HOST:
     items = 100       # assume Github will always return the max items possible
@@ -73,14 +91,21 @@ def populate(url, host):
   """Populate the database with the required number of entries"""
   item_count, link = next_page(url, host)
   items = item_count
-  while items <= ITEMS_REQUIRED:
+  while items <= items_per_host:
+    if VERBOSE: print("Populated", items, link)
     item_count, link = next_page(link, host)
     items += item_count
 
 
 
+if VERBOSE: print("Populating Github repositories from", GH_URL)
 populate(GH_URL, GH_HOST)
+if VERBOSE: print("Populating Bitbucket repositories from", BB_URL)
 populate(BB_URL, BB_HOST)
 
+
+if VERBOSE: print("Committing database changes...")
 db.commit()
 db.close()
+
+print("Finished. Populated database is '{}'".format(DB_PATH))
